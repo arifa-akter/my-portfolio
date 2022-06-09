9057734f02c64ad4534c089ca47d0a9407e777e0
arifa-akter <arifaakterchoity@gmail.com>
2022-06-09 21:17:55 +0600

    add file

--- src/components/HomePage/About.js
+++ src/components/HomePage/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import photo from '../../images/photo.jpg'
+import photo  from '../../images/photo.jpg'
 const About = () => {
     return (
      <section  className='lg:mt-28 lg:mb-28'>
@@ -9,7 +9,7 @@ const About = () => {
              <div className='flex justify-center'>
              <div class="avatar">
               <div class="w-30 mask mask-hexagon">
-                       <img src="https://api.lorem.space/image/face?hash=55350" alt=""/>
+                       <img src={photo} alt=""/>
                </div>
             </div>
              </div>
@@ -18,7 +18,7 @@ const About = () => {
                   <h1 className="mb-3 lg:text-3xl text-2xl">Arifa Akter choity</h1>
                   <h1 className='text-secondary text-xl font-bold'>mern stack developer</h1>
                   <p className='pb-5'>I loved designing things, so I started designing websites to improve my skills. This meant that I had to create something new every week, which was a little better for each project.</p>
-                  <p className='pb-5'>I've heard of WordPress before, but I haven't really looked into it. It all changed when we decided it was time to expand our skill set to realize our website design project.</p>
+                  {/* <p className='pb-5'>I've heard of WordPress before, but I haven't really looked into it. It all changed when we decided it was time to expand our skill set to realize our website design project.</p> */}
                   <p>
                   <i class="fa fa-facebook pr-5 text-xl text-primary" aria-hidden="true"></i>
                   <i class="fa fa-linkedin-square pr-5 text-xl text-primary" aria-hidden="true"></i>
