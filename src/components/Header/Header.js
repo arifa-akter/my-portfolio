5549a7c4813bb49c71a0e190cf49dd645033b455
arifa-akter <arifaakterchoity@gmail.com>
2022-06-10 10:53:15 +0600

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 import './Header.css'
 const Header = () => {
+    const[open ,setOpen]=useState(false)
     return (
      <section className='z-50 header bg-white'>          
     <div className='first-header shadow-xl '>
      <div className='container mx-auto flex justify-between pb-5 pt-5'>
            <div className="logo">
-           <span className='text-[#145A32] font-bold pr-2 pl-1'>Arifa Choity</span>
+           <span className='text-[#145A32] text-xl font-bold pr-2 pl-2'>Arifa Choity</span>
            </div>
-            <div className="login-sign flex items-center ">
-                 <span className='text-[#145A32] font-bold lg:pr-5 pr-2'>About</span>
-                 <span className='text-[#145A32] font-bold lg:pr-5 pr-2'>contact Me</span>
-                 <span className='text-[#145A32] font-bold lg:pr-5 pr-2'>project </span>
-                 
-            </div> 
+           <div className="">
+                <div onClick={()=>setOpen(!open)} className='md:hidden pr-2'>
+                   {open?<i class="fa fa-times text-primary text-2xl" aria-hidden="true"></i>
+                   :<i class="fa fa-bars text-primary text-2xl" aria-hidden="true"></i>}
+                </div>
+                <ul className={`md:flex font-sans font-medium duration-500 
+                ease-in md:static w-full absolute ${open?'top-[72px] right-0 bg-white':'top-[-300px] right-0 bg-white'}`}>
+                    <li className='md:mr-5 md:text-xl text-center md:py-0 p-3 text-primary border-y-2 md:border-y-0'>Home</li>
+                    <li className='md:mr-5 md:text-xl text-center md:py-0 p-3 text-primary border-y-2 md:border-y-0'>About</li>
+                    <li className='md:mr-5 md:text-xl text-center md:py-0 p-3 text-primary border-y-2 md:border-y-0'>project</li>
+                    <li className='md:mr-5 md:text-xl text-center md:py-0 p-3 text-primary border-y-2 md:border-y-0'>Blogs</li>
+                    <li className='md:mr-5 md:text-xl text-center md:py-0 p-3 text-primary border-y-2 md:border-y-0'>contact Me</li>
+                </ul>  
+            </div>
         </div>
      </div>
      </section>
